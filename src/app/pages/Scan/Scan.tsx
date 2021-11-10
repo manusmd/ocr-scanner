d92d1e1029663e5779dbeee62427c2f266c1b088
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-10 11:27:45 +0100

    :sparkles: Add text recognition

--- src/app/pages/Scan/Scan.tsx
+++ src/app/pages/Scan/Scan.tsx
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import Tesseract from 'tesseract.js';
 import ImageInput from '../../components/ImageInput/ImageInput';
 import styles from './Scan.module.css';
 
 function Scan() {
   const [imageURL, setImageURL] = useState<string | null>(null);
+  const [imageText, setImageText] = useState<string | null>(null);
   return (
     <div className={styles.container}>
-      <ImageInput onUpload={setImageURL} />
+      {imageText ? <p>{imageText}</p> : <ImageInput onUpload={setImageURL} />}
       <div className={styles.buttons}>
-        <button className={styles.scanBtn} disabled={imageURL === null}>
+        <button
+          className={styles.scanBtn}
+          disabled={imageURL === null}
+          onClick={() => {
+            if (imageURL) {
+              Tesseract.recognize(imageURL, 'eng', {
+                logger: (message) => console.log(message.progress),
+              }).then((result) => setImageText(result.data.text));
+            }
+          }}
+        >
           Scan
         </button>
         <button className={styles.skipBtn}>Skip</button>
