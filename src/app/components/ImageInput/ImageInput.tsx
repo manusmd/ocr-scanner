bd47d7510ca008618692f2037a54082dddb19e36
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-10 10:15:36 +0100

    :sparkles: Add dynamic image display

--- src/app/components/ImageInput/ImageInput.tsx
+++ src/app/components/ImageInput/ImageInput.tsx
@@ -1,29 +1,36 @@
 import React, { ChangeEvent, useState } from 'react';
 import styles from './ImageInput.module.css';
 
-function ImageInput() {
+type ImageInputProps = {
+  onUpload: (url: string) => void;
+};
+
+function ImageInput({ onUpload }: ImageInputProps) {
   const [imageURL, setImageURL] = useState<string | null>(null);
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log('hallo');
     if (!event.target.files || event.target.files.length === 0) {
       return;
     }
     const file = event.target.files[0];
-    const imageURL = URL.createObjectURL(file);
-    setImageURL(imageURL);
-    console.log(file);
+    const newImageURL = URL.createObjectURL(file);
+
+    setImageURL(newImageURL);
+    onUpload(newImageURL);
   };
 
   return (
-    <label className={styles.fileUpload}>
+    <label>
       <input
         className={styles.input}
         type="file"
         onChange={handleChange}
         accept="image/*"
       />
-      <img className={styles.uploadImage} src="src/assets/uploadImage.png" />
-      {imageURL && <img src={imageURL} alt="" />}
+      {imageURL ? (
+        <img className={styles.uploadImage} src={imageURL} />
+      ) : (
+        <img className={styles.uploadImage} src="src/assets/uploadImage.png" />
+      )}
     </label>
   );
 }
