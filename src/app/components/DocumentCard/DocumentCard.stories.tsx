cfdd8ba00bee0554601f84b204a6445dedd04c00
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-11 21:02:33 +0100

    :sparkles: Add documentCard component

---
+++ src/app/components/DocumentCard/DocumentCard.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import DocumentCard from './DocumentCard';
+
+export default {
+  component: DocumentCard,
+  title: 'Component/DocumentCard',
+};
+
+export const Default = () => <DocumentCard />;
---
+++ src/app/components/DocumentCard/DocumentCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './DocumentCard.module.css';
+
+export default function addDocumentCard() {
+  return (
+    <div className={styles.container}>
+      <img src="" alt="Recognized Image" />
+      <p>Picture Title</p>
+    </div>
+  );
+}
