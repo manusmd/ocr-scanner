7c58fc527f530e910fe7e2bc31761735618d3081
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-14 16:54:23 +0100

    :sparkles: Add documents page basic structure

---
+++ src/app/pages/Documents/Documents.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Documents from './Documents';
+
+export default {
+  component: Documents,
+  title: 'Pages/Documents',
+};
+
+export const Default = () => <Documents />;
---
+++ src/app/pages/Documents/Documents.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Documents.module.css';
+
+export default function Documents() {
+  return <p className={styles.title}>WAS GEHT</p>;
+}
